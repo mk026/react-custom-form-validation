8ed2f851cd970a3e9b1e7f08f979d2fc48b5cc9b
Mikhail <mk0602604@gmail.com>
2023-04-17 23:39:27 +0300

    Refactor text-field component

--- src/components/ui/text-field/TextField.tsx
+++ src/components/ui/text-field/TextField.tsx
@@ -1,7 +1,21 @@
 import { FC, InputHTMLAttributes } from "react";
 
-const TextField: FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
-  return <input {...props} />;
+interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: boolean;
+  message?: string;
+}
+
+const TextField: FC<TextFieldProps> = ({ label, error, message, ...props }) => {
+  return (
+    <>
+      <label style={{ color: error ? "red" : "white" }}>
+        {label}
+        <input {...props} />
+      </label>
+      {message && <span style={{ color: "darkgray" }}>{message}</span>}
+    </>
+  );
 };
 
 export default TextField;
