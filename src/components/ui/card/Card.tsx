1f2c63173184152029d69c1c93fe008c39527a33
Mikhail <mk0602604@gmail.com>
2023-04-30 23:18:19 +0300

    Add styles for card component

--- src/components/ui/card/Card.tsx
+++ src/components/ui/card/Card.tsx
@@ -1,7 +1,18 @@
-import { FC } from "react";
+import { FC, HTMLAttributes, PropsWithChildren } from "react";
 
-const Card: FC = () => {
-  return <div>Card</div>;
+import classes from "./Card.module.css";
+
+interface CardProps extends PropsWithChildren, HTMLAttributes<HTMLDivElement> {}
+
+const Card: FC<CardProps> = ({ children, className, ...props }) => {
+  return (
+    <div
+      className={className ? classes.card + " " + className : classes.card}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Card;
