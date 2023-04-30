272801e33b54d8bf7bd88f3e28dee1e9a17dfcc8
Mikhail <mk0602604@gmail.com>
2023-04-30 23:18:43 +0300

    Add styles for signup-form component

--- src/components/signup-form/SignupForm.tsx
+++ src/components/signup-form/SignupForm.tsx
@@ -1,60 +1,65 @@
 import { FC } from "react";
 
+import { useSignupForm } from "../../hooks/useSignupForm";
 import Form from "../ui/form";
 import TextField from "../ui/text-field";
 import Button from "../ui/button";
-import { useSignupForm } from "../../hooks/useSignupForm";
+import Card from "../ui/card/Card";
+
+import classes from "./SignupForm.module.css";
 
 const FormExample: FC = () => {
   const { handleSubmit, handleReset, errors, register, isValid } =
     useSignupForm();
 
   return (
-    <Form onSubmit={handleSubmit} onReset={handleReset}>
-      <TextField
-        {...register("name")}
-        label="Name"
-        error={!!errors["name"]}
-        message={errors["name"]}
-      />
-      <TextField
-        {...register("age")}
-        label="Age"
-        error={!!errors["age"]}
-        message={errors["age"]}
-      />
-      <TextField
-        {...register("password")}
-        type="password"
-        label="Password"
-        error={!!errors["password"]}
-        message={errors["password"]}
-      />
-      <TextField
-        {...register("confirmPassword")}
-        type="password"
-        label="Password"
-        error={!!errors["confirmPassword"]}
-        message={errors["confirmPassword"]}
-      />
-      <TextField
-        {...register("email")}
-        type="email"
-        label="Email"
-        error={!!errors["email"]}
-        message={errors["email"]}
-      />
-      <TextField
-        {...register("bio")}
-        label="About yourself"
-        error={!!errors["bio"]}
-        message={errors["bio"]}
-      />
-      <Button type="submit" disabled={!isValid}>
-        Submit
-      </Button>
-      <Button type="reset">Reset</Button>
-    </Form>
+    <Card className={classes.wrapper}>
+      <Form onSubmit={handleSubmit} onReset={handleReset}>
+        <TextField
+          {...register("name")}
+          label="Name"
+          error={!!errors["name"]}
+          message={errors["name"]}
+        />
+        <TextField
+          {...register("age")}
+          label="Age"
+          error={!!errors["age"]}
+          message={errors["age"]}
+        />
+        <TextField
+          {...register("password")}
+          type="password"
+          label="Password"
+          error={!!errors["password"]}
+          message={errors["password"]}
+        />
+        <TextField
+          {...register("confirmPassword")}
+          type="password"
+          label="Password"
+          error={!!errors["confirmPassword"]}
+          message={errors["confirmPassword"]}
+        />
+        <TextField
+          {...register("email")}
+          type="email"
+          label="Email"
+          error={!!errors["email"]}
+          message={errors["email"]}
+        />
+        <TextField
+          {...register("bio")}
+          label="About yourself"
+          error={!!errors["bio"]}
+          message={errors["bio"]}
+        />
+        <Button type="submit" disabled={!isValid}>
+          Submit
+        </Button>
+        <Button type="reset">Reset</Button>
+      </Form>
+    </Card>
   );
 };
 
