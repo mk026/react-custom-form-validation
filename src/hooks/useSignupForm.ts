47fd5fbfb4cd0dafd0ab85880768e637cda461ff
Mikhail <mk0602604@gmail.com>
2023-05-06 22:12:13 +0300

    Show notification on successful signup-form submit

--- src/hooks/useSignupForm.ts
+++ src/hooks/useSignupForm.ts
@@ -1,11 +1,15 @@
 import { useForm } from "../lib/hooks/useForm";
 import { signupValidationSchema } from "../validation/signupValidationSchema";
+import { useNotification } from "./useNotification";
 
 export const useSignupForm = () => {
   const { handleSubmit, ...formState } = useForm(signupValidationSchema);
+  const { setData, toggleNotification } = useNotification();
 
   const submitHandler = (values: any) => {
     console.log(values);
+    setData(values);
+    toggleNotification();
   };
 
   return {
