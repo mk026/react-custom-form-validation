4eae36cbe5b40a342ec93441339e7c2bfb34883c
Mikhail <mk0602604@gmail.com>
2023-05-06 22:11:44 +0300

    Create notification component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import SignupForm from "./components/signup-form";
+import Notification from "./components/notification";
 import Layout from "./components/layout";
 
 function App() {
   return (
     <Layout>
       <SignupForm />
+      <Notification />
     </Layout>
   );
 }
---
+++ src/components/notification/Notification.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+
+import { useNotification } from "../../hooks/useNotification";
+import Modal from "../ui/modal";
+import Button from "../ui/button";
+
+import classes from "./Notification.module.css";
+
+const Notification: FC = () => {
+  const { isOpen, toggleNotification, data } = useNotification();
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={toggleNotification}
+      className={classes.notifiaction}
+    >
+      <h3>Successfuly submitted form</h3>
+      {Object.entries(data).map(([name, value]) => (
+        <p key={name}>{`${name}: ${value}`}</p>
+      ))}
+      <Button onClick={toggleNotification}>Close</Button>
+    </Modal>
+  );
+};
+
+export default Notification;
