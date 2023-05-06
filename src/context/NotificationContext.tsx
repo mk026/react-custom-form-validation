1303c21f17ea4ce9a310f01a7ca49dfa663d3843
Mikhail <mk0602604@gmail.com>
2023-05-06 22:09:14 +0300

    Create notification context

---
+++ src/context/NotificationContext.tsx
@@ -0,0 +1,27 @@
+import { FC, PropsWithChildren, createContext, useState } from "react";
+
+export interface INotificationContext {
+  isOpen: boolean;
+  toggleNotification: () => void;
+  setData: (...args: any[]) => void;
+  data: Record<string, string>;
+}
+
+export const NotificationContext = createContext({} as INotificationContext);
+
+const NotificationProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState({});
+
+  const toggleNotification = () => setIsOpen((prev) => !prev);
+
+  const value = { isOpen, toggleNotification, data, setData };
+
+  return (
+    <NotificationContext.Provider value={value}>
+      {children}
+    </NotificationContext.Provider>
+  );
+};
+
+export default NotificationProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import NotificationProvider from "./context/NotificationContext";
 
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <NotificationProvider>
+      <App />
+    </NotificationProvider>
   </React.StrictMode>
 );
