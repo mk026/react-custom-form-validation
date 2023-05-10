378eed304ecf0751acb699251baa5a6538d3865d
Mikhail <mk0602604@gmail.com>
2023-05-11 01:52:05 +0300

    Refactor header component markup

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,9 +4,9 @@ import classes from "./Header.module.css";
 
 const Header: FC = () => {
   return (
-    <div className={classes.header}>
+    <header className={classes.header}>
       <h1 className={classes.title}>React Custom Form Validation</h1>
-    </div>
+    </header>
   );
 };
 
