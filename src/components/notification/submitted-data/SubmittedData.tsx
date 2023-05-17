7ef7d6b8ec01bbe86a8b448897e70ce96f992380
Mikhail <mk0602604@gmail.com>
2023-05-17 22:55:16 +0300

    Add markup in submitted-data component

--- src/components/notification/submitted-data/SubmittedData.tsx
+++ src/components/notification/submitted-data/SubmittedData.tsx
@@ -1,7 +1,31 @@
 import { FC } from "react";
 
-const SubmittedData: FC = () => {
-  return <div>SubmittedData</div>;
+interface SubmittedDataProps {
+  data: Record<string, string>;
+}
+
+const SubmittedData: FC<SubmittedDataProps> = ({ data }) => {
+  return (
+    <div>
+      <table>
+        <caption>Submitted values</caption>
+        <thead>
+          <tr>
+            <th>Field Name</th>
+            <th>Field Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(data).map(([name, value]) => (
+            <tr key={name}>
+              <td>{name}</td>
+              <td>{value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default SubmittedData;
